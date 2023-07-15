d19dc60531b41d032de625563fdbcec06432f227
Joey Ng <joey.ng@segment.com>
2023-07-15 17:28:13 +0800

    Initial Commit

---
+++ segment_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  segment_ios
+//
+//  Created by Joey Ng on 15/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
